c047ecd7036acf9faf262daf0f4352cf882a6a7a
Theatheistmonk <tiwariamit2503@gmail.com>
2026-08-24 13:27:16 +0530

    Code changed by dhruv

--- src/robot_bringup/setup.py
+++ src/robot_bringup/setup.py
@@ -12,7 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Config lives at the workspace root (../../config), not inside this
+        # package, so all ROS packages in the workspace can share one place
+        # for tunable yaml params.
+        (os.path.join('share', package_name, 'config'), glob('../../config/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
     ],
     install_requires=['setuptools'],
@@ -23,7 +26,7 @@ setup(
     license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'serial_bridge_node = robot_bringup.serial_bridge_node:main',
+            'serial_bridge_node = robot_bringup.serial_bridge:main',
         ],
     },
 )
